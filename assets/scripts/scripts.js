46feaebd242b9d3d94809657fe53029d6a6cebd8
SJE-1 <sarahjadeevenden@gmail.com>
2021-02-18 13:19:12 +0000

    fixed typo

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -19,70 +19,70 @@ let play = function(){document.getElementById("audio").play()}
  const cardArray= [
 {
 	name: 'black',
-    img: 'img/black-dot.png'
+    img: 'assets/img/black-dot.png'
 },
 {
 	name: 'black',
-    img: 'img/black-dot.png'
+    img: 'assets/img/black-dot.png'
 },
 {	
 	name: 'aqua',
-	img: 'img/aqua-dot.png'
+	img: 'assets/img/aqua-dot.png'
 }, 
 {	
 	name: 'aqua',
-	img: 'img/aqua-dot.png'
+	img: 'assets/img/aqua-dot.png'
 }, 
 {
 	name: 'blue',
-	img: 'img/blue-dot.png'
+	img: 'assets/img/aqua-dot.png'
 },
 {
 	name: 'blue',
-	img: 'img/blue-dot.png'
+	img: 'assets/img/aqua-dot.png'
 },
 { 
 	name: 'red',
-	img: 'img/red-dot.png'
+	img: 'assets/img/red-dot.png'
 }, 
 { 
 	name: 'red',
-	img: 'img/red-dot.png'
+	img: 'assets/img/red-dot.png'
 }, 
 {
 	name: 'green',
-	img: 'img/green-dot.png'
+	img: 'assets/img/green-dot.png'
 }, 
 {
 	name: 'green',
-	img: 'img/green-dot.png'
+	img: 'assets/img/green-dot.png'
 }, 
 {
 	name: 'orange',
-	img: 'img/orange-dot.png'
+	img: 'assets/img/green-dot.png'
 },
 {
 	name: 'orange',
-	img: 'img/orange-dot.png'
+	img: 'assets/img/green-dot.png'
 },
 { 
 	name: 'purple',
-	img: 'img/purple-dot.png'
+	img: 'assets/img/purple-dot.png'
 },
 { 
 	name: 'purple',
-	img: 'img/purple-dot.png'
+	img: 'assets/img/purple-dot.png'
 },
 { 
 	name: 'pink', 
-	img: 'img/pink-dot.png'
+	img: 'assets/img/pink-dot.png'
 }, 
 { 
 	name: 'pink', 
-	img: 'img/pink-dot.png'
+	img: 'assets/img/pink-dot.png'
 }, 
 ]; 
-
+document.addEventListener('DOMContentLoaded', () => {
 
   cardArray.sort(() => 0.5 - Math.random())
 
@@ -96,7 +96,7 @@ let play = function(){document.getElementById("audio").play()}
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
       const card = document.createElement('img')
-      card.setAttribute('src', 'img/back.png')
+      card.setAttribute('src', 'assets/img/back.png')
       card.setAttribute('data-id', i)
       card.addEventListener('click', flipCard)
       grid.appendChild(card)
@@ -110,20 +110,20 @@ let play = function(){document.getElementById("audio").play()}
     const optionTwoId = cardsChosenId[1]
     
     if(optionOneId == optionTwoId) {
-      cards[optionOneId].setAttribute('src', 'img/back.png')
-      cards[optionTwoId].setAttribute('src', 'img/back.png')
+      cards[optionOneId].setAttribute('src', 'assets/img/back.png')
+      cards[optionTwoId].setAttribute('src', 'assets/img/back.png')
       alert('You have clicked the same image!')
     }
     else if (cardsChosen[0] === cardsChosen[1]) {
       alert('You found a match')
-      cards[optionOneId].setAttribute('src', '')
-      cards[optionTwoId].setAttribute('src', 'img/white.png')
+       cards[optionOneId].setAttribute('src', 'assets/img/white.png')
+      cards[optionTwoId].setAttribute('src', 'assets/img/white.png')
       cards[optionOneId].removeEventListener('click', flipCard)
       cards[optionTwoId].removeEventListener('click', flipCard)
       cardsWon.push(cardsChosen)
     } else {
-      cards[optionOneId].setAttribute('src', 'img/../back.png')
-      cards[optionTwoId].setAttribute('src', 'img/back.png')
+      cards[optionOneId].setAttribute('src', 'assets/img/back.png')
+      cards[optionTwoId].setAttribute('src', 'assets/img/back.png')
       alert('Sorry, try again')
     }
     cardsChosen = []
@@ -145,44 +145,5 @@ let play = function(){document.getElementById("audio").play()}
     }
   }
 
-  createBoard();
-
-
-
-createBoard();
-//THE MODAL//
-// Taken from Bootstrap //
-$('#exampleModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget); // Button that triggered the modal
-  var recipient = button.data('whatever'); // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this);
-  modal.find('.modal-title').text('New message to ' + recipient);
-  modal.find('.modal-body input').val(recipient);
-});
-
-$('#myModal').modal('hide');
-
-function newFunction() {
-    createBoard();
-}
-
-// Countdown for the game
-document.addEventListener('DOMContentLoaded', () => {
-    const timeLeftDisplay = document.querySelector('#time-left');
-    const startBtn = document.querySelector('#start-button');
-    let timeLeft = 60;
-// function loop that does a countdown which stops at 0
-    function countDown() {
-        setInterval(function() {    
-            if(timeLeft <= 0 ){
-                clearInterval(timeLeft = 0);
-            }
-
-            timeLeftDisplay.innerHTML = timeLeft 
-            timeLeft -= 1;
-        }, 1000);
-        }
-    startBtn.addEventListener('click', countDown);
-    });
+   createBoard()
+})
